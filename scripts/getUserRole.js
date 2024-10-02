7e43804a88c6f69573b8571fbfe5d5a57331077a
Faris Fadhail <farisfadhail@gmail.com>
2024-10-02 21:29:56 +0700

    feat: add get user role

---
+++ scripts/getUserRole.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+	const evoteFactory = await hre.ethers.getContractFactory("Evote");
+	const ev = await evoteFactory.attach("0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9");
+
+	const userAddress = "0xlEaDbeefdeadbeefdeadbeefdeadbeefdeadbeef";
+	const result = await ev.getUserRole(userAddress);
+
+	console.log(`User ${userAddress} has role: ${result}`);
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
