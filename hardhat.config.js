6e0bcd42323945983eba6029b012be691e0ef60d
Faris Fadhail <farisfadhail@gmail.com>
2024-10-06 22:46:59 +0700

    add: infura network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { vars } = require("hardhat/config");
+
+const INFURA_API_KEY = vars.get(process.env.INFURA_API_KEY);
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+	solidity: "0.8.27",
+	networks: {
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+		},
+	},
 };
