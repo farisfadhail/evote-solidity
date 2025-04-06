976011d30a88341872d9f498576c7ad9de09bbaa
Faris Fadhail <farisfadhail@gmail.com>
2025-04-06 10:49:40 +0700

    fix: update deploy code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,23 @@
 const hre = require("hardhat");
 
 async function main() {
-	const evoteFactory = await hre.ethers.getContractFactory("Evote");
-	const ev = await evoteFactory.deploy();
+	// const evoteFactory = await hre.ethers.getContractFactory("Evote");
+	// const ev = await evoteFactory.deploy();
+	// await ev.waitForDeployment();
 
-	console.log(`Evote deployed to: ${await ev.getAddress()}`);
+	// console.log(`Evote deployed to: ${await ev.getAddress()}`);
+
+	const [deployer] = await hre.ethers.getSigners();
+
+	console.log(`Deploying contract with account: ${deployer.address}`);
+
+	const evoteFactory = await hre.ethers.getContractFactory("Evote", deployer);
+	const contract = await evoteFactory.deploy("Hello, Web3!");
+
+	console.log(`Waiting for contract deployment...`);
+	await contract.deployed();
+
+	console.log(`Contract deployed at: ${contract.address}`);
 }
 
 main().catch((error) => {
