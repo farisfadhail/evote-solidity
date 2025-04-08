5be4e55be9d6b49f39c1516651d575d18fdd5483
Faris Fadhail <farisfadhail@gmail.com>
2025-04-08 10:28:16 +0700

    fix: update deploy using ethers 6

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,22 @@
 const hre = require("hardhat");
+const { ethers } = hre;
 
 async function main() {
 	const deployerPrivateKey = process.env.PRIVATE_KEY;
-	const provider = new hre.ethers.providers.JsonRpcProvider(process.env.INFURA_SEPOLIA_URL);
-	const deployer = new hre.ethers.Wallet(deployerPrivateKey, provider);
+	const provider = new ethers.JsonRpcProvider(process.env.INFURA_SEPOLIA_URL);
+	const deployer = new ethers.Wallet(deployerPrivateKey, provider);
+
 	console.log(`Deploying contracts with account: ${deployer.address}`);
 
 	console.log(`Using Infura URL: ${process.env.INFURA_SEPOLIA_URL}`);
 
-	const evoteFactory = await hre.ethers.getContractFactory("Evote", deployer);
+	const evoteFactory = await ethers.getContractFactory("Evote", deployer);
 	const contract = await evoteFactory.deploy();
 
 	console.log(`Waiting for contract deployment...`);
-	await contract.deployed();
+	await contract.waitForDeployment();
 
-	console.log(`Contract deployed at: ${contract.address}`);
+	console.log(`Contract deployed at: ${await contract.getAddress()}`);
 }
 
 main().catch((error) => {
